e086271e1a4d2ae0e2181c977009f06bd47bdd3a
Jeremiah <jeremiah.sandoval@gmail.com>
2018-06-06 21:55:49 +0800

    Add original site

---
+++ artencraft/artencraft-original/atrencraft/javascript/second-js.js
@@ -0,0 +1,44 @@
+                var rrPartnerId = "59ad14a4c7d013c210ea4946";
+                var rrApi = {};
+                var rrApiOnReady = rrApiOnReady || [];
+                rrApi.addToBasket = rrApi.order = rrApi.categoryView = rrApi.view =
+                rrApi.recomMouseDown = rrApi.recomAddToCart = function () {
+                };
+                (function (d) {
+                var ref = d.getElementsByTagName('script')[0];
+                var apiJs, apiJsId = 'rrApi-jssdk';
+                if (d.getElementById(apiJsId)) return;
+                apiJs = d.createElement('script');
+                apiJs.id = apiJsId;
+                apiJs.async = true;
+                apiJs.src = "http://cdn.retailrocket.net/content/javascript/tracking.js";
+                ref.parentNode.insertBefore(apiJs, ref);
+                }(document));
+                </script> <noscript> 
+                <iframe src="#" height="0" width="0" style="display:none;visibility:hidden"></iframe> 
+                </noscript> 
+
+                <script>(function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+                });
+                var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src =
+                'http://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+                })(window, document, 'script', 'dataLayer', 'GTM-KC352W');</script> <div id="fb-root"></div> <script>(function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "http://connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.9";
+                fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script> <script type="text/javascript">
+                dataLayer.push({
+                'viewItem':
+                                    206974
+                                });

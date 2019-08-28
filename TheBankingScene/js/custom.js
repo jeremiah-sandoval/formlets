c7e786009721253f6fd74b3eac5f15b0a8b5e850
Jeremiah Sandoval <jeremiah.sandoval@gmail.com>
2019-08-28 12:38:57 +0800

    CSS and JS

--- TheBankingScene/js/custom.js
+++ TheBankingScene/js/custom.js
@@ -18,11 +18,13 @@ Should be equal the the height of the header
 $(window).scroll(function(){
   if ($(window).scrollTop() >= 200) {
       $('#header-2').addClass('fixed-header');
+      $('#content').css("padding-top", "130px");
       $('#home-page #header').invisible();
       $('#home-page #header-2').show();
   }
   else {
       $('#header-2').removeClass('fixed-header');
+      $('#content').css("padding-top", "0px");
       $('#home-page #header').visible();
       $('#home-page #header-2').hide();
   }

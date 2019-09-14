c02eb232e2bfd5e6123246bb04cfeb18d91f8c91
Jeremiah Sandoval <jeremiahsandoval@Jeremiahs-MacBook-Pro.local>
2019-09-14 11:04:48 +0800

    Fix Issues

--- TheBankingScene/js/custom.js
+++ TheBankingScene/js/custom.js
@@ -24,14 +24,14 @@ Should be equal the the height of the header
 ========================================== */
 $(window).scroll(function(){
   if ($(window).scrollTop() >= 200) {
-      $('#header-2').addClass('fixed-header');
-      $('#content').css("padding-top", "130px");
+      $('#home-page #header-2').addClass('fixed-header');
+      $('#content.home-content').css("padding-top", "130px");
       $('#home-page #header').invisible();
       $('#home-page #header-2').show();
   }
   else {
-      $('#header-2').removeClass('fixed-header');
-      $('#content').css("padding-top", "0px");
+      $('#home-page #header-2').removeClass('fixed-header');
+      $('#content.home-content').css("padding-top", "0px");
       $('#home-page #header').visible();
       $('#home-page #header-2').hide();
   }
